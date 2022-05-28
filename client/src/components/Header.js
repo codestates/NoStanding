58c2a6da97cd96b6d04623e30411ace113d46a71
gamemakerr <geakura123@gmail.com>
2022-05-28 15:59:31 +0900

    5월 28일 중간

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faBook,faAddressCard} from '@fortawesome/free-solid-svg-icons'
+import {faBook,faAddressCard,faMagnifyingGlass,faBell} from '@fortawesome/free-solid-svg-icons'
 import {fa0} from '@fortawesome/free-regular-svg-icons'
 import styled from 'styled-components'
 
 const Navbar = styled.nav`
    background-color: aqua;
-
+   display: flex;
+   flex-direction: row;
+   justify-content: space-between;
 `
 const Logo =styled.div`
 
 `
 
 const Menu =styled.div`
-
+  display:flex;
+  flex-direction: row;
 `
-const Search = styled.input`
-
+const Search = styled.div`
+   height:30%;
+   
+   align-items: center;
+   justify-content: center;
 `
 function Header() {
   return(
@@ -25,8 +31,14 @@ function Header() {
         <Logo>
           <a href=''><FontAwesomeIcon icon={faBook} size='4x'></FontAwesomeIcon></a>
         </Logo>
+        <Search >
+          <input defaultValue='가게이름을 입력하세요'></input>
+          <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
+        </Search>
         <Menu>
-          
+          <button>로그인</button>
+          <button>회원가입</button>
+          <FontAwesomeIcon icon={faBell}></FontAwesomeIcon>
         </Menu>
       </Navbar>
         
