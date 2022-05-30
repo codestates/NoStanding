f4ef1f1964ef329eb763e45667dff569b3cb1e72
Sun970324 <ysw5202222@gmail.com>
2022-05-30 14:50:59 +0900

    feat: 틀완성90%

--- client/src/components/master/UserInfo.js
+++ client/src/components/master/UserInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function UserInfo() {
-
+  
 }
 
 export default UserInfo
--- client/src/pages/Mypage.js
+++ client/src/pages/Mypage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
 import SideBar from '../components/SideBar';
