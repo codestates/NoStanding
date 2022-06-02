9fbdfd6493e8de9dee748b1638fe40424aa2e10f
Sun970324 <ysw5202222@gmail.com>
2022-06-02 13:57:49 +0900

    feat 메인 완료

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -14,13 +14,23 @@ const FlexRow = styled.div`
   flex-direction: row;
   width: 100%;
   justify-content: space-around;
+  align-items: center;
   border: 2px solid black;
   height: 5vh;
   align-items: center;
   div {
-    width: auto;
+    height: 100%;
+    flex-grow: 1;
+    text-align: center;
+    border-left: 2px solid black;
   }
 `;
+const CategoryList = styled.div`
+  background-color: ${(props) => String(props.idx) === props.backgroundOn? 'rgba(0, 0, 0, 0.2)':null};
+`;
+const CategortCityList = styled.div`
+background-color: ${(props) => String(props.idx) === props.backgroundCity? 'rgba(0, 0, 0, 0.2)':null};
+`
 const ListView = styled.ul`
   display: flex;
   flex-direction: row;
@@ -28,15 +38,76 @@ const ListView = styled.ul`
   justify-content: center;
 `;
 function Main() {
+  const category = ['음식', '카페', '미용'];
+  const categoryCity = [
+    '서울',
+    '부산',
+    '인천',
+    '대구',
+    '광주',
+    '대전',
+    '울산',
+    '제주',
+  ];
   const [shop, setShop] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [chooseCategory, setChooseCategory] = useState('');
+  const [chooseCategoryCity, setChooseCategoryCity] = useState('');
+  const [backgroundOn, setBackgroundOn] = useState('');
+  const [backgroundCity, setBackgroundCity] = useState('');
+
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_API_URL}/`).then((resp) => {
       setShop(resp.data.data);
       setIsLoading(false);
-      console.log(resp.data.data);
     });
   }, []);
+
+  useEffect(() => {
+    chooseCategory !== '' && chooseCategory !== ''
+      ? axios
+          .get(
+            `${process.env.REACT_APP_API_URL}/category?shop_category=${chooseCategory}&shop_category_city=${chooseCategoryCity}`
+          )
+          .then((resp) => setShop(resp.data.data))
+      : chooseCategory === ''
+      ? axios
+          .get(
+            `${process.env.REACT_APP_API_URL}/category?shop_category_city=${chooseCategoryCity}`
+          )
+          .then((resp) => setShop(resp.data.data))
+      : chooseCategoryCity === ''
+      ? axios
+          .get(
+            `${process.env.REACT_APP_API_URL}/category?shop_category=${chooseCategory}`
+          )
+          .then((resp) => setShop(resp.data.data))
+      : axios.get(`${process.env.REACT_APP_API_URL}/`).then((resp) => {
+          setShop(resp.data.data);
+        });
+  }, [chooseCategory, chooseCategoryCity]);
+
+  const clickCategory = (value, idx) => {
+    idx = String(idx)
+    if(value !== chooseCategory){
+    setChooseCategory(value);
+    setBackgroundOn(idx)
+    }else {
+      setChooseCategory('');
+    setBackgroundOn('')
+    }
+  };
+
+  const clickCategoryCity = (value, idx) => {
+    idx = String(idx)
+    if(value !== chooseCategoryCity){
+    setChooseCategoryCity(value);
+    setBackgroundCity(idx)
+    }else {
+      setChooseCategoryCity('');
+    setBackgroundCity('')
+    }
+  };
   return (
     <>
       <div>
@@ -44,19 +115,27 @@ function Main() {
       </div>
       <FlexCol>
         <FlexRow>
-          <div>음식</div>
-          <div>카페</div>
-          <div>미용</div>
+          {category.map((category, idx) => {
+            return (
+              <CategoryList key={idx} idx={idx} backgroundOn={backgroundOn} onClick={() => clickCategory(category,idx)}>
+                {category}
+              </CategoryList>
+            );
+          })}
         </FlexRow>
         <FlexRow>
-          <div>서울</div>
-          <div>부산</div>
-          <div>인천</div>
-          <div>대구</div>
-          <div>광주</div>
-          <div>대전</div>
-          <div>울산</div>
-          <div>제주</div>
+          {categoryCity.map((category, idx) => {
+            return (
+              <CategortCityList
+                key={idx}
+                idx={idx}
+                backgroundCity={backgroundCity}
+                onClick={() => clickCategoryCity(category,idx)}
+              >
+                {category}
+              </CategortCityList>
+            );
+          })}
         </FlexRow>
       </FlexCol>
       <ListView>
