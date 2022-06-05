3936e5221893eb87ae62293877d1e2efc1daa851
Sun970324 <ysw5202222@gmail.com>
2022-06-05 22:18:44 +0900

    feat:지도도API

---
+++ client/src/components/Map.js
@@ -0,0 +1,26 @@
+import React, { useEffect,useRef } from "react";
+
+const { kakao } = window;
+
+const options = {
+  center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+  level: 3, //지도의 레벨(확대, 축소 정도)
+};
+const Map = () => {
+  const container = useRef(null); //지도를 담을 영역의 DOM 레퍼런스
+
+  useEffect(() => {
+    new kakao.maps.Map(container.current, options); //지도 생성 및 객체 리턴
+    return () => {};
+  }, []);
+
+  return (
+    <div
+      className="map"
+      style={{ width: "500px", height: "500px" }}
+      ref={container}
+    ></div>
+  );
+};
+
+export default Map;
