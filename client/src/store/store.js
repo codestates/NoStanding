a2867dfb6d000802555e8894a0a46e870ca5e867
Sun970324 <ysw5202222@gmail.com>
2022-06-05 17:11:27 +0900

    feat: 로그인유지

---
+++ client/src/store/store.js
@@ -0,0 +1,74 @@
+import { createSlice, configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+// import storageSession from 'redux-persist/lib/storage/session';
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["loginInfo", "loginState"],
+};
+
+const loginInfo = createSlice({
+  name: "userInfo",
+  initialState: { userInfo: {} },
+  reducers: {
+    getUserInfo: (state, action) => {
+      state.userInfo = action.payload;
+    },
+    deleteUserInfo: (state, action) => {
+     return state.userInfo = {}
+    }
+  },
+});
+
+const loginState = createSlice({
+  name: "userLogin",
+  initialState: { userLoginState: false },
+  reducers: {
+    getUserLogin: (state, action) => {
+      state.userLoginState = true;
+    },
+    getUserLogout: (state, action) => {
+      state.userLoginState = false;
+    },
+  },
+});
+
+const shopSearch = createSlice({
+  name: "searchInfo",
+  initialState: { shopSearchInfo: "" },
+  reducers: {
+    getShopSearch: (state, action) => {
+      state.shopSearchInfo = action.payload;
+    },
+  },
+});
+const rootReducer = combineReducers({
+  loginInfo: loginInfo.reducer,
+  loginState: loginState.reducer,
+  shopSearch: shopSearch.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export default store;
+export const { getUserLogout, getUserLogin } = loginState.actions;
+export const { getShopSearch } = shopSearch.actions;
+export const { getUserInfo,deleteUserInfo } = loginInfo.actions;
--- server/controllers/main/category.js
+++ server/controllers/main/category.js
@@ -10,7 +10,6 @@ module.exports = {
     // 가게 이름 , 주소 ,
     try {
       const { shop_category, shop_category_city } = req.query;
-      console.log(req.query);
       const shopInfo = await Models.Shop.findAll({
         include: [
           {
@@ -34,7 +33,7 @@ module.exports = {
           {
             model: Models.Bookmark,
             as: 'Bookmarks',
-            attributes: ['ismarked'],
+            attributes: ['is_marked'],
           },
           {
             model: Models.Review,
@@ -45,10 +44,11 @@ module.exports = {
         attributes: ['id'],
       });
 
-      res.status(200).send({ data: shopInfo, message: '정보 전달 완료' });
+      return res
+        .status(200)
+        .send({ data: shopInfo, message: '정보 전달 완료' });
     } catch (err) {
-      console.log(err);
-      res.status(500).send({ message: 'Server Error' });
+      return res.status(500).send({ message: 'Server Error' });
     }
   },
 };
