a1e49051fd06e094408c0e64e3991d6817571a01
Sangkan-K <gimsanghan53@gmail.com>
2022-06-18 16:16:46 +0900

    fix

--- client/src/components/user/ReviewInfo.js
+++ client/src/components/user/ReviewInfo.js
@@ -6,19 +6,19 @@ const Container = styled.div`
   width: 90%;
   display: flex;
   flex-direction: row;
-  border: 2px solid rgb(21,64,99);
+  border: 2px solid rgb(21, 64, 99);
   border-radius: 0.5rem;
   margin-bottom: 2rem;
 `;
 const ShopNameDiv = styled.div`
   font-size: larger;
   font-weight: bold;
-`
+`;
 const DateDiv = styled.div`
   font-size: small;
-  color: rgb(85,85,85);
+  color: rgb(85, 85, 85);
   margin-bottom: 1rem;
-`
+`;
 const Img = styled.img`
   width: 4em;
   height: 4em;
@@ -35,11 +35,11 @@ const Button = styled.button`
   margin: 1em;
   width: 7em;
   height: 2em;
-  background-color: rgb(21,64,99);
+  background-color: rgb(21, 64, 99);
   color: white;
   border-radius: 0.5rem;
   align-self: flex-start;
-  :hover{
+  :hover {
     transform: scale(1.05);
     background-color: tomato;
   }
@@ -52,8 +52,8 @@ function ReviewInfo({ data, getReviewData }) {
     const parsing = await JSON.parse(data.image_src);
     if (parsing.length !== 0) {
       setImage(parsing);
-    }else {
-      setImage([{location: '/img/default.png', key:1}])
+    } else {
+      setImage([{ location: "/img/default.png", key: 1 }]);
     }
     setLoding(true);
   }, []);
@@ -71,7 +71,7 @@ function ReviewInfo({ data, getReviewData }) {
             })
             .then((resp) => {
               getReviewData();
-              alert('리뷰가 삭제되었습니다.')
+              alert("리뷰가 삭제되었습니다.");
             });
         }
       });
@@ -90,7 +90,11 @@ function ReviewInfo({ data, getReviewData }) {
           </div>
           <Div>
             <ShopNameDiv>{data.shop_name}</ShopNameDiv>
-            <DateDiv>{data.createdAt.split('T')[0]+' '+data.createdAt.split('T')[1].split('.')[0]}</DateDiv>
+            <DateDiv>
+              {data.createdAt.split("T")[0] +
+                " " +
+                data.createdAt.split("T")[1].split(".")[0]}
+            </DateDiv>
             <div>{data.contents}</div>
           </Div>
           <Button onClick={clickDeleteBtn}>리뷰 삭제</Button>
